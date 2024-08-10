6566a9ead58be00371b59858f1bb10fd6bcfabb9
Arthur Renard <renarda57@gmail.com>
2024-08-10 16:55:41 +0200

    Two sum

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -4,5 +4,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i, j = 0, 0
-        for i in
+        i, j = 0, 1
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                print (nums[i] + nums[j])
+                if (nums[i] + nums[j] == target) and (j>i):
+                    return [i, j]
+
+Solution.twoSum(Solution, [0,4,3,0], 0)
